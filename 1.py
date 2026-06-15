bf8b5a4571d5f2ae1eec73948542aa9180e70548
nayakdn <nayakdebendu2023@gmail.com>
2026-06-15 11:08:52 +0530

    vesion

--- 1.py
+++ 1.py
@@ -5,5 +5,12 @@
 #pip3 install uvicorn
 #uvicorn 15main:app --reload
 # git init
-#git add .
-# git commit -m " FastAPI "
+#git add . (git add  file namee) for sending the code again 
+
+# git commit -m " FastAPI "  for sending the file again 
+# git branch -M main 
+#git remote add origin __link___
+#git push -u origin main  for sending the file again 
+# http://127.0.0.1:8000/docs
+# control + c to end the backend session 
+# for making the changes then existing file run three codes which we have to add the file and add . only should be used 
